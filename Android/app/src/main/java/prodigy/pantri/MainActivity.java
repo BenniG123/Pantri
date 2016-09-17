d4f90132573e743cbe84dec484b9520bcfb9c3ae
Benjamin Williams <Benjamin Williams>
2016-09-16 22:14:27 -0500

    Nav bar and git ignore

--- Android/app/src/main/java/prodigy/pantri/MainActivity.java
+++ Android/app/src/main/java/prodigy/pantri/MainActivity.java
@@ -80,17 +80,17 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_home) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_add_food) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_view_pantry) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_cook) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_shopping_list) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_settings) {
 
         }
 
