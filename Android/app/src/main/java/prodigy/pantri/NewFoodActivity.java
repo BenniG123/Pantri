4b46873920bf2731317053debdb808d194c94c9b
Benjamin Williams <Benjamin Williams>
2016-09-17 15:38:23 -0500

    Updated new ingredient

--- Android/app/src/main/java/prodigy/pantri/NewFoodActivity.java
+++ Android/app/src/main/java/prodigy/pantri/NewFoodActivity.java
@@ -3,24 +3,43 @@ package prodigy.pantri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class NewFoodActivity extends AppCompatActivity {
+
+    int quantity = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_food);
+
+        Spinner spinner = (Spinner) findViewById(R.id.category);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.groups_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
     }
 
     public void subQuantity(View v) {
+        if (quantity > 0) {
+            quantity--;
+        }
 
+        ((TextView) findViewById(R.id.quantity)).setText(String.valueOf(quantity));
     }
 
     public void addQuantity(View v) {
-
+        quantity++;
+        ((TextView) findViewById(R.id.quantity)).setText(String.valueOf(quantity));
     }
 
     public void submit(View v) {
-
+        // TODO - REST Call to add ingredient to server
+        finish();
     }
 }
